c65c900bbf72a67d9b77a060f2f7b795b06a0bdf
Abhi088 <abhicoding8@gmail.com>
2022-04-26 23:51:19 +0530

    Encrypting password

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,28 +1,37 @@
 const express = require('express');
 const User = require('../models/User');
 const jwt = require('jsonwebtoken');
-const verifyToken = require('../middleware/auth');
+const bcrypt = require('bcrypt');
 
 const router = express.Router();
 
 router.post("/signup", async (req, res) => {
-    const user = new User(req.body);
-    await user.save().then((data) => {
-        return res.status(201).send(data);
-    }).catch((err) => {
-        return res.status(422).send(err);
+    const userData = req.body;
+    bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(userData['password'], salt, async (err, hash) => {
+            userData['password'] = hash;
+            const user = new User(userData);
+            await user.save().then((data) => {
+                return res.status(201).send(data);
+            }).catch((err) => {
+                return res.status(422).send(err);
+            });
+        });
     });
+
 });
 
 router.post("/login", async (req, res) => {
     const { username, password } = req.body;
     await User.findOne({ username }).then((user) => {
         if (user == null) return res.status(404).send("Invalid credentials");
-        if (user['password'] == password) {
-            const token = jwt.sign({ exp: Math.floor(Date.now() / 1000) + (60 * 60), data: user['username'] }, "private");
-            return res.status(200).send({ token, me: user });
-        }
-        return res.status(401).send("Invalid Credentials");
+        bcrypt.compare(password, user.password, (err, result) => {
+            if (result) {
+                const token = jwt.sign({ exp: Math.floor(Date.now() / 1000) + (60 * 60), data: user['username'] }, "private");
+                return res.status(200).send({ token, me: user });
+            }
+            else return res.status(401).send("Invalid Credentials");
+        });
     });
 })
 
