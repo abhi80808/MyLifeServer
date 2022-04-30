877997293080a6798e6a2e728b0f911251e62a85
Abhi088 <abhicoding8@gmail.com>
2022-04-30 12:50:48 +0530

    Added self transfer function and get net-worth

--- src/controllers/fund.js
+++ src/controllers/fund.js
@@ -4,6 +4,13 @@ const verifyToken = require('../middleware/auth');
 
 const router = express.Router();
 
+router.get("/net-worth", verifyToken, async (req, res, next) => {
+    const funds = req.finance.funds;
+    let net_worth = 0
+    funds.forEach(fund => net_worth += fund.balance);
+    return res.status(200).json({net_worth});
+})
+
 router.post("/fund/storage/create", verifyToken, async (req, res, next) => {
     const fundStorage = new FundStorage(req.body);
     fundStorage.save().then((data) => {
@@ -17,5 +24,23 @@ router.post("/fund/storage/create", verifyToken, async (req, res, next) => {
         return res.status(422).json(err);
     });
 });
+
+router.put("/fund/selftransfer", verifyToken, async (req, res, next) => {
+    const {transferFromId, transferToId, amount} = req.body;
+    if(transferFromId === transferToId) return res.status(422).json({message: "Choose different account to transfer to"});
+    const funds = req.finance.funds;
+    let transferFrom={}, transferTo={};
+    for(let f of funds) {
+        if(f.id === transferFromId) transferFrom = f;
+        else if(f.id === transferToId) transferTo = f;
+    }
+    transferFrom.balance -= amount;
+    transferTo.balance += amount;
+    transferFrom.save().then((data) => {
+        transferTo.save().then((data) => {
+            return res.status(200).json({message: "Transfer successful"});
+        });
+    });
+})
 
 module.exports = router;
--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -4,8 +4,9 @@ const Finance = require('../models/Finance');
 
 module.exports = verifyToken = async (req, res, next) => {
     const token = req.headers.authorization;
+    if(!token) return res.status(401).json({message: "You are not authorized to perform this task"});
     const username = jwt.verify(token, 'private')['data'];
     req.user = await User.findOne({username}).populate('finance');
-    req.finance = await Finance.findOne({_id: req.user.finance._id});
+    req.finance = await Finance.findOne({_id: req.user.finance._id}).populate('funds');
     next();
 }
